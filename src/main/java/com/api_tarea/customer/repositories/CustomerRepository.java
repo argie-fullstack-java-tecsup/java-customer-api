d99ea1090a4a9218885820acd4612abc2042f5e6
argierincon <argierincon@gmail.com>
2026-09-21 19:04:52 -0500

    docs: add comments explaining CustomerRepository

--- src/main/java/com/api_tarea/customer/repositories/CustomerRepository.java
+++ src/main/java/com/api_tarea/customer/repositories/CustomerRepository.java
@@ -3,7 +3,8 @@ package com.api_tarea.customer.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api_tarea.customer.entities.Customer;
-
+// Conecta el servicio con la DB
 public interface CustomerRepository
-        extends JpaRepository<Customer, Long> {
+  // Extiende JpaRepository y estos ejecutan el SQL
+    extends JpaRepository<Customer, Long> {
 }
