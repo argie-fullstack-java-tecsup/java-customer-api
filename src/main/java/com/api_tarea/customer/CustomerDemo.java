1dc2144efd8b3bbaf80e5a6967c6d758615099cd
argierincon <argierincon@gmail.com>
2026-09-21 19:42:36 -0500

    refactor: drop bc_java_tecsup package layer

--- src/main/java/com/api_tarea/customer/CustomerDemo.java
+++ src/main/java/com/api_tarea/customer/CustomerDemo.java
@@ -1,4 +1,4 @@
-package com.bc_java_tecsup.api_tarea.customer;
+package com.api_tarea.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/api_tarea/customer/CustomerService.java
+++ src/main/java/com/api_tarea/customer/CustomerService.java
@@ -1,4 +1,4 @@
-package com.bc_java_tecsup.api_tarea.customer;
+package com.api_tarea.customer;
 
 import org.springframework.stereotype.Service;
 
